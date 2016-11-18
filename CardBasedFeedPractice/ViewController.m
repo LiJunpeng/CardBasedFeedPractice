cb3b721c521164135999213c062ac716e4d46e18
LILouis <lijunpeng91@gmail.com>
2016-11-18 05:15:56 -0500

    update images

--- CardBasedFeedPractice/ViewController.m
+++ CardBasedFeedPractice/ViewController.m
@@ -16,12 +16,22 @@
 
 UITableView *tableView;
 NSMutableArray *test_array;
+NSMutableArray *username_array;
+NSMutableArray *avatar_array;
+NSMutableArray *post_array;
+NSMutableArray *location_array;
+NSMutableArray *viewer_number_array;
 
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view, typically from a nib.
     
     test_array = @[@"r1", @"r2"];
+    username_array = @[@"Donald Trump", @"Hilary Clinton"];
+    avatar_array = @[@"avatar_01.jpg", @"avatar_02.jpg"];
+    post_array = @[@"post_01.jpg", @"post_02.jpg"];
+    location_array = @[@"D.C", @"NYC"];
+    viewer_number_array = @[@"112358", @"1248"];
     
     tableView=[[UITableView alloc]init];
     tableView.frame = CGRectMake(0,0,[[UIScreen mainScreen] bounds].size.width,[[UIScreen mainScreen] bounds].size.height);
@@ -59,14 +69,11 @@ NSMutableArray *test_array;
     {
         cell = [[UITableViewCell alloc] initWithStyle:UITableViewCellStyleDefault reuseIdentifier:CellIdentifier];
     }
-    //cell.textLabel.text=[test_array objectAtIndex:indexPath.row];
     
     cell.frame = CGRectMake(0,0,[[UIScreen mainScreen] bounds].size.width,200);
     
-
-    
     UIImageView *userAvatarView = [[UIImageView alloc] initWithFrame:CGRectZero];
-    [userAvatarView setImage:[UIImage imageNamed:@"avatar_01.png"]];
+    [userAvatarView setImage:[UIImage imageNamed:avatar_array[indexPath.row]]];
     userAvatarView.frame = CGRectMake(0,0,60,60);
     userAvatarView.center = CGPointMake(40,40);
     userAvatarView.layer.cornerRadius = userAvatarView.frame.size.width / 2;
@@ -75,21 +82,21 @@ NSMutableArray *test_array;
     
     UILabel *username_label = [[UILabel alloc] initWithFrame:CGRectMake(0, 0, 200, 20)];
     username_label.center = CGPointMake(175,25);
-    username_label.text = @"Donal Trump!!!!";
+    username_label.text = username_array[indexPath.row];
     [username_label setBackgroundColor:[UIColor clearColor]]; // transparent label background
     [username_label setFont:[UIFont boldSystemFontOfSize:20.0]];
     [cell.contentView addSubview:username_label];
     
     UILabel *location_label = [[UILabel alloc] initWithFrame:CGRectMake(0, 0, 200, 20)];
     location_label.center = CGPointMake(175,50);
-    location_label.text = @"D.C >";
+    location_label.text = location_array[indexPath.row];
     [location_label setBackgroundColor:[UIColor clearColor]]; // transparent label background
     [location_label setFont:[UIFont boldSystemFontOfSize:16.0]];
     [cell.contentView addSubview:location_label];
     
     UILabel *viewer_number_label = [[UILabel alloc] initWithFrame:CGRectMake(0, 0, 100, 20)];
     viewer_number_label.center = CGPointMake([[UIScreen mainScreen] bounds].size.width,25);
-    viewer_number_label.text = @"11235";
+    viewer_number_label.text = viewer_number_array[indexPath.row];
     [viewer_number_label setBackgroundColor:[UIColor clearColor]]; // transparent label background
     [viewer_number_label setFont:[UIFont boldSystemFontOfSize:14.0]];
     [cell.contentView addSubview:viewer_number_label];
@@ -102,17 +109,12 @@ NSMutableArray *test_array;
     [cell.contentView addSubview:watch_label];
     
     UIImageView *bgView = [[UIImageView alloc] initWithFrame:CGRectZero];
-    [bgView setImage:[UIImage imageNamed:@"title_bar.png"]];
+    [bgView setImage:[UIImage imageNamed:post_array[indexPath.row]]];
     bgView.frame = CGRectMake(0,0,[[UIScreen mainScreen] bounds].size.width,[[UIScreen mainScreen] bounds].size.width);
     bgView.center = CGPointMake([[UIScreen mainScreen] bounds].size.width / 2, [[UIScreen mainScreen] bounds].size.width / 2 + 80);
     [cell.contentView addSubview:bgView];
     
-
-    
-    
-    //[(UILabel *)[cell.contentView viewWithTag:1] setText:[data1 objectAtIndex:indexPath.row]];
-    
-    
+ 
     return cell;
 }
 
@@ -126,9 +128,6 @@ NSMutableArray *test_array;
 {
     return [[UIScreen mainScreen] bounds].size.width + 80;
 }
-
-
-
 
 
 - (void)didReceiveMemoryWarning {
